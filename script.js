f3857cb827023b352f9a043c952cf0bbefdc7b6d
CsiPA0723 <csizipeti0723@gmail.com>
2018-11-15 18:44:51 +0100

    Some sh*t.

---
+++ script.js
@@ -0,0 +1,13 @@
+function init() {
+    
+}
+
+function gameSearch() {
+
+}
+
+function userSearch() {
+    var uSelect = document.getElementById("user-select").value;
+    var uSelect2 = document.getElementById("user-select2").value;
+    window.location = "./respons.html";
+}
