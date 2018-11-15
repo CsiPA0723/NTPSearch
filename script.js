e12c65edc3f5e09be8a83213990c3b9463c14cd5
CsiPA0723 <csizipeti0723@gmail.com>
2018-11-15 19:03:17 +0100

    meh.

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function init() {
-    
+
 }
 
 function gameSearch() {
+    var gSearch = document.getElementById("user-search").value;
+    var result = document.getElementById("result");
+    var xhttp = new XMLHttpRequest();
 
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4) {
+            if(this.status == 200) {
+                result.innerHTML = this.responseText;
+            }
+            document.getElementById("demo").innerHTML = "...";
+        }
+    }
+    xhttp.
+    xhttp.open("GET", "http://www.boardgamegeek.com/xmlapi/search?search="+encodeURI(gSearch), true)
+    xhttp.send();
 }
 
 function userSearch() {
+    var uSearch = document.getElementById("user-search").value;
     var uSelect = document.getElementById("user-select").value;
     var uSelect2 = document.getElementById("user-select2").value;
-    window.location = "./respons.html";
+    var result = document.getElementById("result");
+
+
 }
